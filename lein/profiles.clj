3ef69c21689c7ff131472bde1ea89183cd155ac1
shyam sunder k <surya46584@gmail.com>
2015-09-18 18:14:39 -0400

    add profiles.clj

---
+++ lein/profiles.clj
@@ -0,0 +1,6 @@
+{:user
+ {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
+            [refactor-nrepl "1.2.0-SNAPSHOT"]
+            [lein-cljfmt "0.2.1" :exclusions [org.clojure/clojure]]
+            [lein-ancient "0.6.7"]]
+  :repl-options {:timeout 300000}}}
