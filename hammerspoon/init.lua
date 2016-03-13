5a9b38a32ef9aa3133aa423a6708b8ed8b2bc6d5
shyam sunder k <surya46584@gmail.com>
2016-03-13 16:51:19 -0400

    add hammerspoon and slate configuration

---
+++ hammerspoon/init.lua
@@ -0,0 +1,346 @@
+-----------------------------------------------
+-- Set up
+-----------------------------------------------
+
+local hyper = {"shift", "cmd"}
+local mash = {"shift", "cmd","alt"}
+hs.window.animationDuration = 0
+
+require("hs.application")
+require("hs.window")
+
+-- Get list of screens and refresh that list whenever screens are plugged or unplugged:
+local screens = hs.screen.allScreens()
+local screenwatcher = hs.screen.watcher.new(function()
+	screens = hs.screen.allScreens()
+end)
+screenwatcher:start()
+
+-- Resize window for chunk of screen.
+-- For x and y: use 0 to expand fully in that dimension, 0.5 to expand halfway
+-- For w and h: use 1 for full, 0.5 for half
+function push(x, y, w, h)
+	local win = hs.window.focusedWindow()
+	local f = win:frame()
+	local screen = win:screen()
+	local max = screen:frame()
+
+	f.x = max.x + (max.w*x)
+	f.y = max.y + (max.h*y)
+	f.w = max.w*w
+	f.h = max.h*h
+	win:setFrame(f)
+end
+
+-- Move to monitor x. Checks to make sure monitor exists, if not moves to last monitor that exists
+function moveToMonitor(x)
+	local win = hs.window.focusedWindow()
+	local newScreen = nil
+	while not newScreen do
+		newScreen = screens[x]
+		x = x-1
+	end
+
+	win:moveToScreen(newScreen)
+end
+
+-----------------------------------------------
+-- hyper left for left one half window
+-----------------------------------------------
+
+hs.hotkey.bind(hyper, 'Left', function()
+    if hs.window.focusedWindow() then
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x
+        f.y = max.y
+        f.w = max.w / 2
+        f.h = max.h
+        win:setFrame(f)
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+-----------------------------------------------
+-- hyper Right for right one half window
+-----------------------------------------------
+
+hs.hotkey.bind(hyper, 'Right', function()
+    if hs.window.focusedWindow() then
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x + (max.w / 2)
+        f.y = max.y
+        f.w = max.w / 2
+        f.h = max.h
+        win:setFrame(f)
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+-----------------------------------------------
+-- hyper f for fullscreen
+-----------------------------------------------
+
+hs.hotkey.bind(hyper, 'f', function()
+    if hs.window.focusedWindow() then
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x
+        f.y = max.y
+        f.w = max.w
+        f.h = max.h
+        win:setFrame(f)
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+hs.hotkey.bind(mash, 'f', function()
+        local win = hs.window.focusedWindow()
+        if win ~= nil then
+            win:setFullScreen(not win:isFullScreen())
+        end
+    end)
+-----------------------------------------------
+--top left one quarter window
+-----------------------------------------------
+
+hs.hotkey.bind(mash, 'left', function()
+    if hs.window.focusedWindow() then
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x
+        f.y = max.y
+        f.w = max.w / 2
+        f.h = max.h / 2
+        win:setFrame(f)
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+
+
+-----------------------------------------------
+--top right one quarter window
+-----------------------------------------------
+
+hs.hotkey.bind(mash, 'up', function()
+    if hs.window.focusedWindow() then
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x + (max.w / 2)
+        f.y = max.y
+        f.w = max.w / 2
+        f.h = max.h / 2
+        win:setFrame(f)
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+-----------------------------------------------
+-- bottom right one quarter window
+-----------------------------------------------
+
+hs.hotkey.bind(mash, 'right', function()
+    if hs.window.focusedWindow() then
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x + (max.w / 2)
+        f.y = max.y + (max.h / 2)
+        f.w = max.w / 2
+        f.h = max.h / 2
+        win:setFrame(f)
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+-----------------------------------------------
+-- bottom left one quarter window
+-----------------------------------------------
+
+hs.hotkey.bind(mash, 'down', function()
+    if hs.window.focusedWindow() then
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x
+        f.y = max.y + (max.h / 2)
+        f.w = max.w / 2
+        f.h = max.h / 2
+        win:setFrame(f)
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+-----------------------------------------------
+-- Bottom
+-----------------------------------------------
+
+hs.hotkey.bind(hyper,"down", function() push(0,0.5,1,0.5) end)
+
+-----------------------------------------------
+-- Top
+-----------------------------------------------
+
+hs.hotkey.bind(hyper,"up", function()	push(0,0,1,0.5) end)
+
+-----------------------------------------------
+-- Reload config on write
+-----------------------------------------------
+
+function reload_config(files)
+    hs.reload()
+end
+hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload_config):start()
+hs.alert.show("Config loaded")
+
+-----------------------------------------------
+-- Hyper i to show window hints
+-----------------------------------------------
+
+hs.hotkey.bind(hyper, '.', function()
+    hs.hints.windowHints()
+end)
+
+-----------------------------------------------
+-- Hyper hjkl to switch window focus
+-----------------------------------------------
+
+hs.hotkey.bind(hyper, 'k', function()
+    if hs.window.focusedWindow() then
+        hs.window.focusedWindow():focusWindowNorth()
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+hs.hotkey.bind(hyper, 'j', function()
+    if hs.window.focusedWindow() then
+        hs.window.focusedWindow():focusWindowSouth()
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+hs.hotkey.bind(hyper, 'l', function()
+    if hs.window.focusedWindow() then
+    hs.window.focusedWindow():focusWindowEast()
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+hs.hotkey.bind(hyper, 'h', function()
+    if hs.window.focusedWindow() then
+        hs.window.focusedWindow():focusWindowWest()
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+-------------------------------
+-- Move a window between monitors
+-------------------------------
+hs.hotkey.bind(hyper,"1", function() moveToMonitor(1) end) -- Move to first monitor
+hs.hotkey.bind(hyper,"2", function() moveToMonitor(2) end) -- Move to second monitor
+
+-----------------------------------------
+--- Application Shortcuts
+-----------------------------------
+hs.hotkey.bind(hyper, "C", function() hs.application.launchOrFocus("Google Chrome") end)
+hs.hotkey.bind(hyper, "E", function() hs.application.launchOrFocus("Emacs") end)
+hs.hotkey.bind(hyper, "X", function() hs.application.launchOrFocus("Microsoft Excel") end)
+hs.hotkey.bind(hyper, "S", function() hs.application.launchOrFocus("Safari") end)
+hs.hotkey.bind(hyper, "M", function() hs.application.launchOrFocus("Mail") end)
+hs.hotkey.bind(hyper, "I", function() hs.application.launchOrFocus("iTerm") end)
+hs.hotkey.bind(hyper, "B", function() hs.application.launchOrFocus("iBooks") end)
+hs.hotkey.bind(hyper, "O", function() hs.application.launchOrFocus("Photos") end)
+hs.hotkey.bind(hyper, "G", function() hs.application.launchOrFocus("Messages") end)
+
+-- multi monitor
+hs.hotkey.bind(hyper, 'N', hs.grid.pushWindowNextScreen)
+hs.hotkey.bind(hyper, 'P', hs.grid.pushWindowPrevScreen)
+
+----shrink window
+hs.hotkey.bind(hyper, '-', function()
+  local win = hs.window.focusedWindow()
+  if not win then
+    alertCannotManipulateWindow()
+    return
+  end
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  if f.x + f.w >= (max.w - 5) then
+    -- Aligned to the right
+    f.w = f.w - 20
+    f.x = max.w - f.w
+  elseif (max.w - 10) <= (f.x * 2) + f.w then
+    -- Center window shrinking
+    f.w = f.w - 20
+    f.x = (max.w - f.w) / 2
+  else
+    f.w = f.w - 20
+  end
+
+  win:setFrame(f)
+end)
+
+----Grow Window
+hs.hotkey.bind(hyper, '=',  function()
+  local win = hs.window.focusedWindow()
+  if not win then
+    alertCannotManipulateWindow()
+    return
+  end
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  if f.x == 0 then
+    -- Aligned to the left
+    f.w = f.w + 20
+  elseif f.x + f.w >= (max.w - 5) then
+    -- Aligned to the right
+    f.w = f.w + 20
+    f.x = max.w - f.w
+  elseif (max.w - 10) <= (f.x * 2) + f.w then
+    -- Center window growing
+    f.w = f.w + 20
+    f.x = (max.w - f.w) / 2
+  else
+    -- Everywhere else
+    f.w = f.w + 20
+    f.x = f.x - 10
+  end
+
+  win:setFrame(f)
+end)
---
+++ slate/slate.js
@@ -0,0 +1,72 @@
+// My Slate configuration
+
+// Store current window states.
+var windowStates = {};
+
+// Left screen edge operations.
+var leftBarThird = S.op("push",
+                        {"direction": "left",
+                         "style": "bar-resize:screenSizeX/3"});
+var leftBarHalf = S.op("push",
+                       {"direction": "left",
+                        "style": "bar-resize:screenSizeX/2"});
+var leftBarTwoThirds = S.op("push",
+                            {"direction": "left",
+                             "style": "bar-resize:screenSizeX/3*2"});
+
+// Right screen edge operations.
+var rightBarThird = S.op("push",
+                         {"direction": "right",
+                          "style": "bar-resize:screenSizeX/3"});
+var rightBarHalf = S.op("push",
+                        {"direction": "right",
+                         "style": "bar-resize:screenSizeX/2"});
+var rightBarTwoThirds = S.op("push",
+                             {"direction": "right",
+                              "style": "bar-resize:screenSizeX/3*2"});
+
+/* This closure returns a function that will cycle a window through
+ * the states given, assuming that each state is the string name of a
+ * valid Slate operation object created with `S.op()'.
+ *
+ * The function returned is expected to be bound to a key and thus
+ * accepts one argument, the window object.
+ */
+function getCycleStates(states) {
+  return function(win) {
+    if (win.pid in windowStates &&
+        windowStates[win.pid] !== undefined &&
+        states.indexOf(windowStates[win.pid]) !== -1
+       ) {
+         var nextIndex = (states.indexOf(windowStates[win.pid]) + 1) % 3;
+         var nextState = states[nextIndex];
+         windowStates[win.pid] = nextState;
+       } else {
+         windowStates[win.pid] = states[0];
+       }
+
+    win.doOperation(windowStates[win.pid]);
+  };
+}
+
+// Window cycling functions.
+var leftCycleOp = getCycleStates([leftBarHalf, leftBarThird, leftBarTwoThirds]);
+var rightCycleOp = getCycleStates([rightBarHalf, rightBarThird, rightBarTwoThirds]);
+
+// Bind the window cycling functions.
+S.bind('h:cmd,shift', leftCycleOp);
+S.bind('l:cmd,shift', rightCycleOp);
+
+// Full-screen.
+S.bind(
+  'f:cmd,shift',
+  S.op(
+    'move',
+    {
+      'x': 'screenOriginX',
+      'y': 'screenOriginY',
+      'width': 'screenSizeX',
+      'height': 'screenSizeY'
+    }
+  )
+);
