0366a6e060226c4c4e81ba09a04cf7063e8c71ec
shyam sunder k <surya46584@gmail.com>
2016-08-29 14:39:57 -0400

    update profiles.clj

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user
- {:plugins [[cider/cider-nrepl "0.13.0-SNAPSHOT"]
-            [refactor-nrepl "1.2.0-SNAPSHOT"]
-            [lein-cljfmt "0.2.1" :exclusions [org.clojure/clojure]]
-            [lein-ancient "0.6.7"]]
+ {:plugins [[cider/cider-nrepl "0.13.0"]
+            [refactor-nrepl "2.2.0"]
+            [lein-cljfmt "0.5.3" :exclusions [org.clojure/clojure]]
+            [lein-ancient "0.6.10"]]
   :repl-options {:timeout 300000}}}
