5e66aaee0fc2ab17f924d2a3958b6833e15a2560
shyam sunder k <surya46584@gmail.com>
2016-11-23 18:27:54 -0500

    kwm osx xmod changes

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user
- {:plugins [[cider/cider-nrepl "0.13.0"]
-            [refactor-nrepl "2.2.0"]
+ {:plugins [[cider/cider-nrepl "0.15.0-snapshot"]
+            [refactor-nrepl "2.3.0-SNAPSHOT"]
             [lein-cljfmt "0.5.3" :exclusions [org.clojure/clojure]]
             [lein-ancient "0.6.10"]]
   :repl-options {:timeout 300000}}}
