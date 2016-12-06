68ad16e513803d053524d84cba007db190079cc9
shyam sunder k <surya46584@gmail.com>
2016-12-06 15:44:32 -0500

    Merge branch 'master' of https://github.com/surya46584/dotfiles
    * 'master' of https://github.com/surya46584/dotfiles:
      brew and aliases

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,6 +1,4 @@
-{:user
- {:plugins [[cider/cider-nrepl "0.13.0"]
-            [refactor-nrepl "2.2.0"]
-            [lein-cljfmt "0.5.3" :exclusions [org.clojure/clojure]]
-            [lein-ancient "0.6.10"]]
-  :repl-options {:timeout 300000}}}
+{:user {:plugins [[lein-try "0.4.3"]
+                  [lein-ancient "0.6.10"]
+                  [lein-exec "0.3.6"]]}
+ :repl {:plugins [[cider/cider-nrepl "0.14.0"]]}}
