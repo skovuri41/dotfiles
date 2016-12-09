36ceb432604d87241534de816c58ac5ca8dce695
shyam sunder k <surya46584@gmail.com>
2016-12-08 21:50:18 -0500

    profiles for print-length

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,5 @@
 {:user {:plugins [[lein-try "0.4.3"]
                   [lein-ancient "0.6.10"]
-                  [lein-exec "0.3.6"]]}
+                  [lein-exec "0.3.6"]]
+        :repl-options {:init (set! *print-length* 100)}}}
  :repl {:plugins [[cider/cider-nrepl "0.14.0"]]}}
