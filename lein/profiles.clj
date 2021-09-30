def71ea69dbcad1067895fcc669c3304b3b63f80
shyam sunder k <shaymkovuri3@outlook.com>
2021-09-29 21:58:41 -0400

    reinstall regolit

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,11 +1,11 @@
-{:user {:plugins      [[cider/cider-nrepl "0.24.0"]
-                       [lein-pprint "1.1.2"]
+{:user {:plugins      [[cider/cider-nrepl "0.25.9"]
+                       [lein-pprint "1.3.2"]
                        [lein-try "0.4.3"]
-                       [lein-ancient "0.6.14"]
-                       [lein-exec "0.3.6"]
-                       [lein-kibit "0.1.6"]
-                       [com.jakemccrary/lein-test-refresh "0.20.0"]
-                       [walmartlabs/vizdeps "0.1.6"]]
+                       [lein-ancient "0.7.0"]
+                       [lein-exec "0.3.7"]
+                       [lein-kibit "0.1.8"]
+                       [com.jakemccrary/lein-test-refresh "0.24.1"]
+                       [walmartlabs/vizdeps "0.2.0"]]
         :repl-options {:init (set! *print-length* 100)}
         :dependencies [[clj-kondo "RELEASE"]]
         :aliases      {"clj-kondo" ["run" "-m" "clj-kondo.main"]}}}
